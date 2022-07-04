b761a98639586c67c2f238e5357636d64d2a825a
Serhan <serhanozel07@gmail.com>
2022-07-04 11:55:13 +0300

    seems fine

--- script.js
+++ script.js
@@ -5,71 +5,81 @@ function computerPlay() {
     return item.toUpperCase()
 }
 
-function yourTurn() {
-    let yourSelection = window.prompt("it's your turn. what do you choose to play with?: ")
-    // yourselection variable is not in the global scope, it might be a problem 
-    if (yourSelection.toUpperCase() !== "ROCK" && yourSelection.toUpperCase() !== "PAPER" && yourSelection.toUpperCase() !== "SCISSORS") {
-        alert("you have to type one of these three options: 'rock', 'paper' and 'scissors'")
-    } else if (yourSelection.toUpperCase() === "ROCK" || yourSelection.toUpperCase() === "PAPER" || yourSelection.toUpperCase() === "SCISSORS") {
-        return yourSelection.toUpperCase()
-    }
-}
-
 let playerScore = 0
 let computerScore = 0
 let roundCounter = 0
 
+let currentWinner = '';
+
+const choices = document.querySelector(".choices")
+const scores = document.querySelector(".scores")
+const winner = document.querySelector(".winner")
+
 function singleRound(playerSelection, computerSelection) {
-    if (playerSelection == undefined) {
-        console.log("wrongg!! you've typed in something wrong.")
-    } else if (playerSelection === computerSelection) {
-        console.log(`playerSelection: ${playerSelection}, computerSelection: ${computerSelection}`)
-        console.log("no winner, it's the same")
-        console.log(`playerScore: ${playerScore}, computerScore: ${computerScore}`)
+    if (playerSelection === computerSelection) {
+        currentWinner = "no winner, it's the same"
+        choices.textContent = `${currentWinner} - your choice: ${playerSelection}, computer's choice: ${computerSelection}`
+        roundCounter++
+
+        scores.textContent = `your score: ${playerScore}, computer's score: ${computerScore}`
+        
     } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS" || playerSelection === "PAPER" && computerSelection === "ROCK" || playerSelection === "SCISSORS" && computerSelection === "PAPER") {
-        console.log(`playerSelection: ${playerSelection}, computerSelection: ${computerSelection}`)
-        console.log("hey, you've won!! congrats!!")
+        currentWinner = "hey, you've won!! congrats!!" 
         playerScore++
         roundCounter++
-        console.log(`playerScore: ${playerScore}, computerScore: ${computerScore}`)
+        scores.textContent = `your score: ${playerScore}, computer's score: ${computerScore}`
+        if (playerScore === 5) {
+            winner.textContent = "YOU'VE WON THE WHOLE GAME!!!"
+            return
+        }
+
+        choices.textContent = `${currentWinner} - your choice: ${playerSelection}, computer's choice: ${computerSelection}`
+        
     } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK" || playerSelection === "ROCK" && computerSelection === "PAPER" || playerSelection === "PAPER" && computerSelection === "SCISSORS") {
-        console.log(`playerSelection: ${playerSelection}, computerSelection: ${computerSelection}`)
-        console.log("shit! the computer has won. unlucky :[")
+        currentWinner = "shit! the computer has won. unlucky :["
         computerScore++
         roundCounter++
-        console.log(`playerScore: ${playerScore}, computerScore: ${computerScore}`)
-    }
+        scores.textContent = `your score: ${playerScore}, computer's score: ${computerScore}`
 
+        if (computerScore === 5) {
+            winner.textContent = "YOU'VE lost :("
+            return
+        }
+
+        choices.textContent = `${currentWinner} - your choice: ${playerSelection}, computer's choice: ${computerSelection}`
+        
+    }
 }
+
+function getButtonText ()  {
+    singleRound(this.textContent.toUpperCase(), computerPlay())
+}
+
+
+const buttons = document.querySelectorAll("button")
+buttons.forEach(button => button.addEventListener("click", getButtonText))
+
+
+
 
 //you've not added an algorithm in which when your score is 3, you beat the computer and vice versa
 
-function game() {
-    // for (i = 0; i < 10; i++) {
-    //     singleRound(yourTurn(), computerPlay())
-    //     if (roundCounter === 5) {
-    //         console.log(`playerScore: ${playerScore}, computerScore: ${computerScore}`)
-    //         if (playerScore > computerScore) {
-    //             console.log("YES! YOU'VE WON THE WHOLE FUCKIN' GAME :D")
-    //         } else if (computerScore > playerScore) {
-    //             console.log("YOU'RE COMPLETELY DEFEATED")
-    //         }
-    //     }
-    // }
+// function game() {
 
-    while (roundCounter < 5) {
-        singleRound(yourTurn(), computerPlay())
-    }
-    if (roundCounter === 5) {
-        console.log(`playerScore: ${playerScore}, computerScore: ${computerScore}`)
-        if (playerScore > computerScore) {
-            console.log("YES! YOU'VE WON THE WHOLE FUCKIN' GAME :D")
-        } else if (computerScore > playerScore) {
-            console.log("YOU'RE COMPLETELY DEFEATED")
-        }
-    }
-}
+//     while (roundCounter < 5) {
+//         singleRound(yourTurn(), computerPlay())
+//     }
+//     if (roundCounter === 5) {
+//         console.log(`playerScore: ${playerScore}, computerScore: ${computerScore}`)
+//         if (playerScore > computerScore) {
+//             console.log("YES! YOU'VE WON THE WHOLE FUCKIN' GAME :D")
+//         } else if (computerScore > playerScore) {
+//             console.log("YOU'RE COMPLETELY DEFEATED")
+//         }
+//     } 
+// }
 
-game()
+// game()
 
+// singleRound(yourTurn(), computerPlay())
 
